6ce368fb28d07c75a38b6e396b8508a928699beb
Dadabbe <aym.davias@gmail.com>
2022-01-25 23:22:39 +0100

    Final commit : Few UI changes, trying toasts

--- app/src/main/java/com/example/tt_invoxia/viewmodel/LocationAdapter.kt
+++ app/src/main/java/com/example/tt_invoxia/viewmodel/LocationAdapter.kt
@@ -1,16 +1,18 @@
 package com.example.tt_invoxia.viewmodel
 import android.annotation.SuppressLint
+//import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
+//import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+
 import com.example.tt_invoxia.R
 
 
 class LocationAdapter(private var locationList: MutableList<LocationViewModel>) : RecyclerView.Adapter<LocationAdapter.ViewHolder>() {
-
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
             val view = LayoutInflater.from(parent.context)
                 .inflate(R.layout.card_view_location, parent, false)
@@ -19,13 +21,16 @@ class LocationAdapter(private var locationList: MutableList<LocationViewModel>) 
 
         @SuppressLint("NotifyDataSetChanged")
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
+            //Absence de contexte pour les toasts
+            //val toast = Toast.makeText(,"Location supprimée !", Toast.LENGTH_SHORT)
             val ItemsViewModel = locationList[position]
             holder.textViewAddress.text = ItemsViewModel.address
             holder.textViewDate.text = ItemsViewModel.date.toString()
             holder.deleteButton.setOnClickListener{
                     locationList.removeAt(position)
                     notifyDataSetChanged()
+                    //toast.show()
+
             }
 
         }
@@ -40,4 +45,4 @@ class LocationAdapter(private var locationList: MutableList<LocationViewModel>) 
         }
 
 
-    }
+}
